b8a25c82b6338c774790958c93d987f84e4f937b
devesh0605 <dsaxena_be18@thapar.edu>
2020-12-30 16:00:17 +0530

    coincard created

--- src/componentts/CoinCard.js
+++ src/componentts/CoinCard.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import { StyleSheet } from 'react-native'
-import {images} from '../Utils/CoinICons'
+import { StyleSheet, Text, View } from 'react-native'
+import {images} from '../Utils/CoinIcons'
 
 const styles = StyleSheet.create({
     container:{
@@ -40,7 +40,67 @@ const styles = StyleSheet.create({
     },
     moneySymbol:{
         fontWeight:'bold',
-    }
+    },
+    statisticsContainer:{
+        display:'flex',
+        borderTopColor:'#fafafa',
+        borderTopWidth:2,
+        padding:10,
+        flexDirection:'row',
+        justifyContent:'space-around',
 
-
+    },
+    percentChangePlus:{
+        color:'gren',
+        fontWeight:'bold',
+        marginLeft:5,
+    },
+    percentChangeMinus:{
+        color:'red',
+        fontWeight:'bold',
+        marginLeft:5,
+    },
 })
+
+const {
+    container,
+    image,
+    moneySymbol,
+    upperRow,
+    coinName,
+    coinPrice,
+    coinSymbol,
+    statisticsContainer,
+    separator,
+    percentChangeMinus,
+    percentChangePlus,
+} = styles
+
+const CoinCard = ({symbol,coin_name,price_usd,percent_change_24hr,percent_change_7d})=>{
+    return (
+        <View style={container}>
+            <View style={upperRow}>
+                <Image style={image} source={{uri:images[symbol]}}/>
+                <Text style={coinSymbol}>{symbol}</Text>
+                <Text style={separator}>~</Text>
+                <Text style={coinName}>{coin_name}</Text>
+                <Text style={coinPrice}>{price_usd}
+                <Text style={moneySymbol}> $ </Text>
+                </Text>
+            </View>
+
+            <View style={statisticsContainer}>
+                <Text>24Hr:
+                    <Text style={percent_change_24hr < 0 ? percentChangeMinus : percentChangePlus}>{percent_change_24hr} %</Text>
+                </Text>
+                <Text>7D:
+                    <Text style={percent_change_7d < 0 ? percentChangeMinus : percentChangePlus}>{percent_change_7d} %</Text>
+                </Text>
+            </View>
+
+        </View>
+        
+    )
+}
+
+export default CoinCard
