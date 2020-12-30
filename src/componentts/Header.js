4b94b120019995b8fc816b5d3f94167be4eed645
devesh0605 <dsaxena_be18@thapar.edu>
2020-12-30 18:59:01 +0530

    creted header

--- src/componentts/Header.js
+++ src/componentts/Header.js
@@ -1,2 +1,27 @@
 import React, { Component } from 'react'
-import { StyleSheet, Text, View, ScrollView } from 'react-native'
+import { StyleSheet, Text, View } from 'react-native'
+
+const Header = ()=>{
+    return(
+        <View style={headerContainer}>
+            <Text style={header}>
+                Crypto App
+            </Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    headerContainer:{
+        display:'flex',
+        marginTop:55,
+        alignItems:'center',
+    },
+    header:{
+        fontWeight:'bold',
+        fontSize:20,
+    }
+})
+
+const {headerContainer,header}=styles
+export default Header
