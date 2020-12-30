bd68237df82335707231dfb3522e370eee369d28
devesh0605 <dsaxena_be18@thapar.edu>
2020-12-30 21:28:07 +0530

    added store

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open  to start working on your app!</Text>
+      <Text>Odfsdf!</Text>
       <StatusBar style="auto" />
     </View>
   );
